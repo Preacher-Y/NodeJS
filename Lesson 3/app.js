0d05ff4f5e95d5147db6361e917c95856b2299ab
Preacher-Y <ysheja@gmail.com>
2025-09-15 15:30:23 +0200

    Done Passing in the projects

--- Lesson 3/app.js
+++ Lesson 3/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const path = require('path')
 
 // register view engine
 app.set('view engine', "ejs")
@@ -14,10 +15,19 @@ app.set('views',path.join(__dirname,'./'))
 app.listen(3000)
 
 app.get('/',(req,res)=>{
-    res.sendFile('./index.html',{root:__dirname})
+    const projects = [
+        {title:'Analytics Dashboard', description:'Role‑based insights with charts, filters, and exports..', tools:['React','Node','PostgreSQL']},
+        {title:'Realtime Chat', description:'WebSocket-based messaging with typing indicators.', tools:['TypeScript','WebSocket','Redis']},
+        {title:'E‑Commerce API', description:'Orders, payments, and inventory with clean docs.', tools:['Node','Express','stripe']},
+        {title:'Ansys Receipt Manager', description:'OCR based, retrieving the data from the receipt and exporting the to excel', tools:['React','Supabase','Express','Mindee']},
+        {title:'GlobalRoots', description:'Connect Diaspora mentor and Youth in Rwanda, LinkedIn Profile based.', tools:['React','Python','Express','EmailJS']},
+        {title:'Chef Claude', description:'AI based Chef, providing recipe from the given ingredients', tools:['React','TailwindCSS','TypeScript','Anthropic']}
+        
+    ]
+    res.render('index',{name : 'Yves', fullName: 'Yves Sheja N M ', projects})
 })
 app.get('/project',(req,res)=>{
-    res.sendFile('./project.html',{root:__dirname})
+    res.render('project')
 })
 
 app.get('/about',(req,res)=>{
